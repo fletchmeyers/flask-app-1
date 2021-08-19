f45b337a98648057b6c1742736f47c349e63aab3
Fletcher Meyers <fletchmeyers@gmail.com>
2021-08-19 11:51:26 -0400

    evolutions tables have data and links, still need to be stylized

--- static/js/detail.js
+++ static/js/detail.js
@@ -4,8 +4,11 @@ $(document).ready(function() {
 
     var fullUrl = window.location.href;
     var uri = fullUrl.split("http://127.0.0.1:5000/");
+    var next = parseInt(uri[1]) + 1;
     console.log("work??");
     console.log(uri[1]);
+    
+    
     
 
     $.ajax({
@@ -23,22 +26,53 @@ $(document).ready(function() {
             var img = data["image_url"];
             var name = data["name"];
             var description = data["description"];
+            var types = data["types"];
+            var evolutions = data["evolutions"];
+            console.log(evolutions.length);
+          
+
+
+
+
+
+
+
+
+
+
+            $(".name").append( `<h1>${name}</h1>`);
+            $(".img").append(`<img src="${img}" alt="${name}" width="100%" height="100%">`);
+            $(".description").append(`<p>${description}</p>`);    
+            
+            for (let i = 0; i < types.length; i++) {         
+                $(".types").append(`<li>${types[i]}</li>`);
+            }   
+            
+            for (let i = 0; i < evolutions.length; i++) {
+                $(".evolutionsPokemon")
+                    .append(
+                        `<a href="/${data["evolutions"][i]["id"]}">
+                            <tr>
+                                <td>${data["evolutions"][i]["to"]}</td>
+                            </tr>
+                        <a>`
+                    );
             
 
-            $(".container")
-                .append(
-                    `<div class = 'details'>
-                        <p>${description}</p>
-                        <img src="${img}" alt="${name}" width="80%" height="80%">
-                    </div>`);
-                    
+                $(".evolutionsLevel")
+                    .append(
+                        `<tr><td>${data["evolutions"][i]["level"]}</td></tr>`
+                    );
+            
+                
+             
+                $(".evolutionsMethod")
+                    .append(
+                        `<tr><td>${data["evolutions"][i]["method"]}</td></tr>`
+                    );
+            
+                }
 
-
-
-
-
-
- 
         }
     });
     
